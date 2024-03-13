320e97b21d5d4f93ecded6c74b4228610aff2967
Pratik Pawar <PP7701264@GMAIL.COM>
2024-03-13 06:36:33 +0530

    update index.php
    remove bug

--- query.sql
+++ query.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE IF NOT EXISTS your_database;
+CREATE DATABASE IF NOT EXISTS registration;
 
-USE your_database;
+USE registration;
 
-CREATE TABLE IF NOT EXISTS your_table (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
