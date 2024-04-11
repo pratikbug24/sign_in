7706210e02c6b558b615f7934ab33fd1f81ab1d7
Pratik Pawar <140439634+pratikbug24@users.noreply.github.com>
2024-04-11 07:13:17 +0530

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,9 +1,11 @@
-CREATE DATABASE IF NOT EXISTS registration;
-
-USE registration;
-
-CREATE TABLE IF NOT EXISTS users (
+-- Create a table to store user information
+CREATE TABLE IF NOT EXISTS `users` (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    user_name VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
+
+-- Inserting a sample user into the users table
+INSERT INTO users (user_name,password,name) VALUES
+('Pratik24', 'Pratik@321',"Pratik");
